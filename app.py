44a3ad8902600e4ad8e864df9ba46d3912f0d3a1
Jeremy Hauschildt <jeremy@jhauschildt.com>
2016-09-07 18:12:53 -0400

    Trying out GA

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ def index():
         return redirect(url_for('index'))
 
     #return render_template('index.html', guests=Guest.query.all())
-    return "Hello There!"
+    return "Hello There! <br> <script>(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');  ga('create', 'UA-18747469-1', 'auto');  ga('send', 'pageview');</script>"
 
 
 class Guest(db.Model):
